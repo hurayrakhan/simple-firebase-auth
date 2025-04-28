3264879f9674041452203bcf4cdd1fea5decbac0
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-28 15:29:05 +0600

    added github and google firebase auth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,37 @@
 
-import { GoogleAuthProvider } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../Firebase/firebase.init';
 
 const Login = () => {
 
     const provider = new GoogleAuthProvider();
+    const GithubProvider = new GithubAuthProvider();
+
+    const handleLogin = () => {
+
+        console.log('hello google')
+
+        signInWithPopup(auth, provider)
+        .then(result => {
+            console.log(result)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    const handleGithubLogin = () => {
+
+        signInWithPopup(auth, GithubProvider)
+        .then(result => {
+            console.log(result)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+    }
 
     return (
         <div className='text-center place-items-center mt-36 '>
@@ -18,7 +45,8 @@ const Login = () => {
                 <input type="password" className="input" placeholder="Password" />
 
                 <button className="btn btn-neutral mt-4">Login</button>
-                <button className="btn btn-primary mt-4">Login with Google</button>
+                <button onClick={handleLogin} className="btn btn-primary mt-4">Login with Google</button>
+                <button onClick={handleGithubLogin} className="btn btn-accent mt-4">Login with Github</button>
 
             </fieldset>
         </div>
