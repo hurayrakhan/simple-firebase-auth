ee598b997a10c27e8b106ab3b77411d8cb72a225
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-30 23:31:51 +0600

    added password email login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,5 @@
 
 import { createUserWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
 import { auth } from '../Firebase/firebase.init';
 
 const Login = () => {
@@ -13,57 +12,63 @@ const Login = () => {
         console.log('hello google')
 
         signInWithPopup(auth, provider)
-        .then(result => {
-            console.log(result)
-        })
-        .catch(error => {
-            console.log(error)
-        })
+            .then(result => {
+                console.log(result)
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     const handleGithubLogin = () => {
 
         signInWithPopup(auth, GithubProvider)
-        .then(result => {
-            console.log(result)
-        })
-        .catch(error => {
-            console.log(error)
-        })
+            .then(result => {
+                console.log(result)
+            })
+            .catch(error => {
+                console.log(error)
+            })
 
     }
 
-    const handleEmailPasswordLogin = e => {
+    const handleEmailPasswordLogin = (e) => {
         e.preventDefault();
-        console.log(e.target)
+        
         const email = e.target.email.value
         const password = e.target.password.value
-         
-        createUserWithEmailAndPassword(auth, email, password )
-        .then(result => {
+        
 
-        })
-        .catch(error => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
 
-        })
+            })
+            .catch(error => {
+
+            })
     }
 
     return (
         <div className='text-center place-items-center mt-36 '>
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            <form onSubmit={handleEmailPasswordLogin} className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
                 <legend className="fieldset-legend">Login</legend>
 
                 <label className="label">Email</label>
-                <input type="email" className="input" placeholder="Email" />
+                <input className="input validator" type="email" name='email' required placeholder="mail@site.com" />
 
                 <label className="label">Password</label>
-                <input type="password" className="input" placeholder="Password" />
+                <input type="password" className="input validator" required name='password' placeholder="Password" minLength="8"
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    title="Must be more than 8 characters, including number, symbol, uppercase letter" />
+                
 
-                <button onClick={handleEmailPasswordLogin} className="btn btn-neutral mt-4">Login</button>
-                <button onClick={handleLogin} className="btn btn-primary mt-4">Login with Google</button>
-                <button onClick={handleGithubLogin} className="btn btn-accent mt-4">Login with Github</button>
+                <input className='btn btn-neutral mt-5' type="submit" value="submit" />
 
-            </fieldset>
+
+            </form>
+            <button onClick={handleLogin} className="btn btn-primary mt-4">Login with Google</button>
+            <br />
+            <button onClick={handleGithubLogin} className="btn btn-accent mt-4">Login with Github</button>
         </div>
     );
 };
